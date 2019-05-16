7badd2c5cae8d1785da2a43b13d9afdad568a161
Ristea Stefan <stefanristea1@yahoo.com>
2019-05-16 17:08:07 +0300

    Implemented Homework2

---
+++ Homework2/Homework2/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Homework2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(1, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(754, 409);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(23, 228);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "EditLine";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(23, 180);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "AddLine";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(23, 135);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "DeleteNode";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 316);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "EditNodeName";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "AddNode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(23, 274);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "DeleteLine";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Homework2/Homework2/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework2
+{
+
+    struct Line
+    {
+        public Point p1, p2;
+
+        public bool Equals(Line obj)
+        {
+            if (obj.p1 == this.p1 && obj.p2 == this.p2)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    };
+
+    struct Node
+    {
+        public Rectangle rect;
+        public List<Line> vertices;
+        public Node(Rectangle r)
+        {
+            vertices = new List<Line>();
+            rect = r;
+        }
+    }
+
+
+    public partial class Form1 : Form
+    {
+        public bool isDrawNodePushed;
+        public bool isEditNodePushed;
+        public bool isDeleteNodePushed;
+        public bool isAddLinePushed;
+        public bool isStartAdded;
+        public bool isEditLinePushed;
+        public bool isDeleteLinePushed;
+        public Point lineStart, lineEnd;
+        Dictionary<Label, Rectangle> map = new Dictionary<Label, Rectangle>();
+        Dictionary<Line, Label> map2 = new Dictionary<Line, Label>();
+        Rectangle r1, r2;
+        List<Node> Nodes = new List<Node>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            isDrawNodePushed = false;
+            isEditNodePushed = false;
+            isDeleteNodePushed = false;
+            isAddLinePushed = false;
+            isEditLinePushed = false;
+            isDeleteLinePushed = false;
+            isStartAdded = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isDrawNodePushed = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            isEditNodePushed = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            isDeleteNodePushed = true ;   
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            isEditLinePushed = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            isDeleteLinePushed = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            isAddLinePushed = true;
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            Point point = panel1.PointToClient(Cursor.Position);
+            if (isDrawNodePushed)
+            {
+                Pen sb = new Pen(Color.Black);
+                Pen sw = new Pen(Color.White);
+                Graphics g = panel1.CreateGraphics();
+                Rectangle rect = new Rectangle(point.X, point.Y, 40, 40);
+                Nodes.Add(new Node(rect));
+                g.DrawRectangle(sb, rect);
+
+                Label lbl = new Label();
+                lbl.Text = "1";
+                lbl.AutoSize = true;
+                lbl.ForeColor = Color.Red;
+                lbl.Location = new Point(point.X + 16, point.Y + 25);
+                this.Controls.Add(lbl);
+                lbl.BringToFront();
+                map.Add(lbl, rect);
+                lbl.Click += delegate
+                {
+                    if (isEditNodePushed && textBox1.Text != "")
+                    {
+                        lbl.Text = textBox1.Text;
+                        isEditNodePushed = false;
+                    }
+
+                    if (isDeleteNodePushed)
+                    {
+                        g.DrawRectangle(sw, map[lbl]);
+                        foreach (Node node in Nodes)
+                        {
+                            if (node.rect == map[lbl])
+                            {
+                                foreach (Line line in node.vertices)
+                                {
+                                    g.DrawLine(sw, line.p1, line.p2);
+                                    map2[line].Dispose();
+                                }
+                            }
+                        }
+                        map.Remove(lbl);
+                        lbl.Dispose();
+                        isDeleteNodePushed = false;
+                    }
+
+                    if (isAddLinePushed && isStartAdded)
+                    {
+                        lineEnd.X = point.X;
+                        lineEnd.Y = point.Y;
+                        r2 = map[lbl];
+                        Line l1 = new Line();
+
+                        l1.p1.X = lineStart.X;
+                        l1.p1.Y = lineStart.Y;
+                        l1.p2.X = lineEnd.X;
+                        l1.p2.Y = lineEnd.Y;
+                        foreach (Node node in Nodes)
+                        {
+                            if (node.rect == r1)
+                            {
+                                node.vertices.Add(l1);
+                                Label lbl2 = new Label();
+                                lbl2.Text = "1";
+                                lbl2.AutoSize = true;
+                                lbl2.ForeColor = Color.Red;
+                                lbl2.Location = new Point((l1.p1.X + l1.p2.X)/2, (l1.p1.Y + l1.p2.Y)/2);
+                                this.Controls.Add(lbl2);
+                                lbl2.BringToFront();
+                                map2.Add(node.vertices.LastOrDefault(), lbl2);
+                                lbl2.Click += delegate
+                                {
+                                    if (isEditLinePushed)
+                                        lbl2.Text = textBox1.Text;
+                                    if (isDeleteLinePushed)
+                                    {
+                                        foreach (KeyValuePair<Line, Label> pair in map2)
+                                            if (pair.Value == lbl2)
+                                            {
+                                                g.DrawLine(sw, pair.Key.p1, pair.Key.p2);
+                                                foreach (Node node2 in Nodes)
+                                                {
+                                                    foreach (Line line in node2.vertices)
+                                                        if (line.Equals(pair.Key))
+                                                        {
+                                                            node2.vertices.Remove(line);
+                                                            break;
+                                                        }
+                                                }
+
+                                            }
+                                        lbl2.Dispose();
+                                    }
+                                };
+                            }
+                            if (node.rect == r2)
+                                node.vertices.Add(l1);
+                        }
+
+                        g.DrawLine(sb, lineStart, lineEnd);
+                        isAddLinePushed = false;
+                        isStartAdded = false;
+                    }
+
+                    if (isAddLinePushed && !isStartAdded)
+                    {
+                        lineStart.X = point.X;
+                        lineStart.Y = point.Y;
+                        isStartAdded = true;
+                        r1 = map[lbl];
+                    }
+                };
+
+                isDrawNodePushed = false;
+            }
+        }
+
+    }
+}
